58e2d5fed5be2469871d70096b80ae72a31511ca
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-02 16:01:10 -0400

    count

---
+++ functionprac.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+      $nflTeams[] = "Broncos";
+      $nflTeams[] = "Bears";
+      $nflTeams[] = "Titans";
+
+      echo count($nflTeams);
+    ?>
+
+
+  </body>
+</html>
