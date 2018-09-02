7ca678a62994acb8fb7b815497a39c8018b8531b
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-02 16:09:44 -0400

    my function

--- functionprac.php
+++ functionprac.php
@@ -11,7 +11,16 @@
       $nflTeams[] = "Bears";
       $nflTeams[] = "Titans";
 
-      echo count($nflTeams);
+      echo count($nflTeams); //predeterminied count funciton tells you how many elements are in that array
+
+      //Creating your own funciton
+      echo "<br /> <br />"
+      function distance(){
+        $rate = 50;
+        $time = 4;
+        echo $rate * $time ;
+
+      }
     ?>
 
 
