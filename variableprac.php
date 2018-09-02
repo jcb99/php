e178d021dae17e079a5856f64c09cf4d73bfbb0f
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-02 15:07:43 -0400

    begin

---
+++ variableprac.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<?php
+$hours = 40; //Int data type
+$rate = 20.5; //float
+$name = "John"; //string
+$paperwork = True; //bool
+$date = Null; //null
+
+
+echo $hours; <br />
+echo $rate; <br />
+echo $name; <br />
+echo $paperwork; <br />
+echo $date; <br />
+<br />
+echo $hours * $rate;
+ ?>
+
+  </body>
+</html>
