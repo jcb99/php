5c785565ea6425554db81bddb904f57bffa5b2b5
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-02 15:38:32 -0400

    arrays

---
+++ arrayprac.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+    $states = array("Alaska", "Alabama", "Arkansas");
+
+    echo $states[0];
+    echo $states[1];
+    echo $states [2];
+
+
+     ?>
+  </body>
+</html>
