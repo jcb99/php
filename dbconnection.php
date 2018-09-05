11e1d9840a9a1ee750cfb045a85fee4f406353f0
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-05 11:35:33 -0400

    pass

--- dbconnection.php
+++ dbconnection.php
@@ -2,7 +2,7 @@
 //Setting up a database connection
 $db_host = 'localhost'; //We are never leaving this specific so we are using local host (it is installed on our PHP server already)...Could replace that with an IP
 $db_user = "jake"; //name to login to DB
-$db_password = "password"; //Password to log into mysql
+$db_password = "southhills#"; //Password to log into mysql
 $db_name = "jake"; //name of DB within mysql
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);// This is passing in the 4 parameters defined above..Conn is short for connection
 
