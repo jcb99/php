6621a4eda5cca81de2ccb36068b85492b8071152
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-10 07:57:06 -0400

    commit

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,9 @@
 <?php
 //Setting up a database connection
 $db_host = 'localhost'; //We are never leaving this specific so we are using local host (it is installed on our PHP server already)...Could replace that with an IP
-$db_user = "jake"; //name to login to DB
-$db_password = "southhills#"; //Password to log into mysql
-$db_name = "jake"; //name of DB within mysql
+$db_user = 'jake'; //name to login to DB
+$db_password = 'southhills#'; //Password to log into mysql
+$db_name = 'jake'; //name of DB within mysql
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);// This is passing in the 4 parameters defined above..Conn is short for connection
 
 if ($conn->connect_error) {
