341daa2ae49ae0302e8a55781c660ceb95b73fcc
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-11 10:50:44 -0400

    staged

---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php
+  if (!isset($_SESSION)){ //Check to see if the session global variable...This is better than isset because it can create errors in the back logs...
+                          //If the session variable is not set, then set it..else do nothing
+    session_start();
+  }
+
+  if (!isset($_SESSION['username'])) //someone must be logged in to proceed
+  // you could do this.....die ("No");
+  header('password.php'); //this kicks the user back to the login screen
+  //This can't be executed after HTML has been loaded so it must be before the HTML
+
+//The below is only submitted when someone hits the submit button..does not run as soon as the page loads
+  if (isset($_POST['upload'])) { //This checks to see if post data has been submitted into upload....In the form below the input typ is called upload..Thats what we're referring to
+    $target_dir = "uploads/"; //the target directory of the file...will be on the server in the same PHP directory as this file
+    $target_file = $target_dir . basename($_FILES['upload']['name']); //The actual file name is pulled from the basename...The basename function pulls the rest of the file path out and keeps just the name and extension
+    move_uploaded_file($_FILES['upload']['tmp_name'], $target_file); //moves the uploaded file from memory to this location
+  }
+ ?>
+
+ Upload your file.
+
+ <form action = "" method="post" enctype="multipart/form-data">
+   <!-- We must have the enctype="multipart/form-data in order for the browser to understand that this isn't just normal post data..Otherwise the form won't work"  -->
+   <input type="file" name="upload">
+   <br>
+   <input type="submit" name="Submit">
+ </form>
