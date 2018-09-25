1bed6a63a29afc73c9b4b7f8e3ba49bad25b3709
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-25 10:41:43 -0400

    stage

--- edituser.php
+++ edituser.php
@@ -12,16 +12,17 @@ if(isset($_GET['id']) && $_GET['edit']=="edit"){
 require('dbconnection.php'); //bring in database connection
 $sql = "SELECT * FROM users WHERE userid = " . $_GET['id'];
 $result = $conn->query($sql);
-}
 
 echo "<form action = \"\" method = \"post\">";
 
 while ($row = $result->fetch_assoc()) {
-  echo "<input type=\"text\" disabled value = \"" . $row['userid'] . "\">";
+  echo "<input name = \"userid\" type=\"text\" disabled value = \"" . $row['userid'] . "\">";
   echo "<br />";
-  echo "<input type=\"text\" disabled value = \"" . $row['username'] . "\">";
+  echo "<input name = \"username\" type=\"text\" disabled value = \"" . $row['username'] . "\">";
   echo "<br />";
-  echo "<input type=\"text\" disabled value = \"" . $row['password'] . "\">";
+  echo "<input name = \"password\" type=\"text\" disabled value = \"" . $row['password'] . "\">";
+}
+
 
 }
 
