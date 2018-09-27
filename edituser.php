7fc027f991d142c65817aa165b4e70f118ca4b0d
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-26 21:07:59 -0400

    push

--- edituser.php
+++ edituser.php
@@ -46,19 +46,19 @@ if (isset($_POST['username']) && isset($_POST['submit'])){
   $uname = str_replace(" ", "", $uname);
   //$uname = trim($uname);
 
-      //if ($_POST['username'] != ""){
-          if($uname != ""){
-            $uname = $_POST['username'];
-            $uname = str_replace(" ", "", $uname);
-            //$uname = trim($uname);
-            $update = "UPDATE users SET username = \"" .  $uname . "\" WHERE userid = " . $_GET['id'];
-            $conn->query($update);
-          }
-      //}
+
+      if($uname != ""){
+        $uname = $_POST['username'];
+        $uname = str_replace(" ", "", $uname);
+        //$uname = trim($uname);
+        $update = "UPDATE users SET username = \"" .  $uname . "\" WHERE userid = " . $_GET['id'];
+        $conn->query($update);
+      }
+
 
       else{
         echo "Yo idiot that is not a username";
-    }
+      }
 }
 
 
