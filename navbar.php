baafb999be176fc76303325a09a4e022fd247af4
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-01 19:33:34 -0400

    push

--- navbar.php
+++ navbar.php
@@ -9,9 +9,9 @@
       <?php
       echo (basename($_SERVER['PHP_SELF']) == "password.php") ? "<strong><a href=password.php>|Home</a></strong>" : "<a href=password.php>|Home</a>";
       echo "|&nbsp&nbsp";
-      echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<strong><a href=register.php>|Register|</a></strong>" : "<a href=register.php>|Register|</a>";
+      echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<strong><a href=register.php>|Register</a></strong>" : "<a href=register.php>|Register</a>";
+      echo "|&nbsp&nbsp";
       if(isset($_SESSION['username'])){
-          echo "|&nbsp&nbsp";
           echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<strong><a href=upload.php>|Upload</a></strong>" : "<a href=upload.php>|Upload</a>";
           echo "|&nbsp&nbsp";
           echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<strong><a href=users.php>|Users|</a></strong>" : "<a href=users.php>|Users|</a>";
