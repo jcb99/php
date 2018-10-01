5d24b357cabecc658fd22c5545e6e2734ae23e47
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-01 19:52:16 -0400

    push

--- navbar.php
+++ navbar.php
@@ -7,17 +7,15 @@
   <body>
     <div align="center">
       <?php
-      echo "<div style=\"text-decoration: none\">";
-      echo (basename($_SERVER['PHP_SELF']) == "password.php") ? "<strong><a href=password.php>|Home</a></strong>" : "<a href=password.php>|Home</a>";
+      echo (basename($_SERVER['PHP_SELF']) == "password.php") ? "<strong><a href=password.php style=\"text-decoration: none\">|Home</a></strong>" : "<a href=password.php style=\"text-decoration: none\">|Home</a>";
       echo "|&nbsp&nbsp";
-      echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<strong><a href=register.php>|Register</a></strong>" : "<a href=register.php>|Register</a>";
+      echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<strong><a href=register.php>|Register</a style=\"text-decoration: none\"></strong>" : "<a href=register.php style=\"text-decoration: none\">|Register</a>";
       echo "|&nbsp&nbsp";
       if(isset($_SESSION['username'])){
-          echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<strong><a href=upload.php>|Upload</a></strong>" : "<a href=upload.php>|Upload</a>";
+          echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<strong><a href=upload.php>|Upload</a></strong style=\"text-decoration: none\">" : "<a href=upload.php style=\"text-decoration: none\">|Upload</a>";
           echo "|&nbsp&nbsp";
-          echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<strong><a href=users.php>|Users|</a></strong>" : "<a href=users.php>|Users|</a>";
+          echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<strong><a href=users.php style=\"text-decoration: none\">|Users|</a></strong>" : "<a href=users.php style=\"text-decoration: none\">|Users|</a>";
         }
-      echo "</div>";
        ?>
        <br />
     </div>
