200adb35103f25660ed7c8d47cf22ccc5b5269be
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-01 16:25:57 -0400

    psuh

---
+++ navbar.php
@@ -0,0 +1,4 @@
+<?php
+echo (basename($_SERVER['PHP_SELF']) == "users.php" ? <a href=users.php>Users</a> : <strong><a href=users.php>Users</a></strong>)
+echo "<a href users.php>";
+ ?>
--- users.php
+++ users.php
@@ -36,14 +36,14 @@ $conn->close();
    </head>
    <body>
      <?php
-       if(isset($_SESSION['username'])){ //Show the upload button when logged in
-         echo "<div align=\"center\">";
-         echo "<a href=\"password.php\">Home</a>";
-         echo "<a href=\"register.php\"> | Register</a>";
-         echo "<a href=\"upload.php\"> | Upload</a>";
-         echo "<a href=\"users.php\"> | Users</a>";
-         echo "</div>";
-       }
+       // if(isset($_SESSION['username'])){ //Show the upload button when logged in
+       //   echo "<div align=\"center\">";
+       //   echo "<a href=\"password.php\">Home</a>";
+       //   echo "<a href=\"register.php\"> | Register</a>";
+       //   echo "<a href=\"upload.php\"> | Upload</a>";
+       //   echo "<a href=\"users.php\"> | Users</a>";
+       //   echo "</div>";
+       // }
 
       ?>
 
