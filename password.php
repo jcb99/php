4c38a019d355fafa9251c23f4c3e08cd0631693f
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-01 19:41:41 -0400

    push

--- password.php
+++ password.php
@@ -56,8 +56,9 @@ if(isset ($_POST['username'])){ //executes only if the page has been set
     </div>
 
     <form action="" method="post">
-      <input type="text" name="username" placeholder="Username"> <br />
-      <input type="password" name="password">
+      <h4>Login to the site!</h4>
+      <input type="text" name="username" placeholder="Username"> <br /><br />
+      <input type="password" name="password" placeholder="Password">
       &nbsp&nbsp&nbsp&nbsp&nbsp
       <input type ="submit" value="Login">
       <br />
