294efe548c8745ee8cdde1f03153e662f5b0360f
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-01 16:43:17 -0400

    push

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
 <?php
-echo (basename($_SERVER['PHP_SELF']) == "users.php" ? echo "<a href=\"users.php\">Users</a>" : echo "<strong><a href=\"users.php\">Users</a></strong>");
+echo (basename($_SERVER['PHP_SELF']) == "users.php" ? <a href=\"users.php\">Users</a> : <strong><a href=\"users.php\">Users</a></strong>);
 echo "<a href users.php>";
  ?>
