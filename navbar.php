4e649123021e1275d972d6b0da1639f9bb157076
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-01 17:31:24 -0400

    push

--- navbar.php
+++ navbar.php
@@ -8,10 +8,10 @@
     <div align="center">
       <?php
       echo (basename($_SERVER['PHP_SELF']) == "password.php") ? "<strong><a href=password.php>Home</a></strong>" : "<a href=password.php>Home</a>";
-      echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<strong><a href=register.php>Register</a></strong>" : "<a href=register.php>Register</a>";
+      echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<strong><a href=register.php> | Register</a></strong>" : "<a href=register.php> | Register</a>";
       if(isset($_SESSION['username'])){
-          echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<strong><a href=upload.php>Upload</a></strong>" : "<a href=upload.php>Upload</a>";
-          echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<strong><a href=users.php>Users</a></strong>" : "<a href=users.php>Users</a>";
+          echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<strong><a href=upload.php> | Upload</a></strong>" : "<a href=upload.php> | Upload</a>";
+          echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<strong><a href=users.php> | Users</a></strong>" : "<a href=users.php> | Users</a>";
 
         }
        ?>
