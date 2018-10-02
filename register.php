a6ae4cc2ff25ef7f03117e128fec2923e1e9360b
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-02 10:45:18 -0400

    push

--- register.php
+++ register.php
@@ -2,7 +2,11 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { //THIS SAYS ONLY IF THERE IS ANY POST DATA..sO WE DON'T END UP QUERYING THE DB WHEN WE DON'T NEED TO
   require('dbconnection.php');
   $username = $_POST['username'];
-  $password = $_POST['password'];
+    $username = filter_var($username, FILTER_SANITIZE_STRING); //gRABS POST DATA AND AVOIDS CROSS SITE SCRIPTING AND SQL INJECTION
+    $username = trim($username);  //trims white space off of either side
+    $username = stripslashes($username);//Does not let username contain slashes
+    $uname = str_replace(" ", "", $username); //replaces whitespace with nothing
+  $password = $_POST['password']; //dont need to sanitize password bc it is being hashed
   $password = password_hash($password, PASSWORD_BCRYPT); //This is for PHP version 5.5 and up....5.4 and down doesn't recognize this password hash function
   $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')"; //Insert whatever is in the variables above
   $conn->query($sql); //to run the query
