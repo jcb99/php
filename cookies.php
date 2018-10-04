13adcd9708ec2cfe883919fc9d3ad3d7d94c1c72
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-04 10:50:24 -0400

    push

--- cookies.php
+++ cookies.php
@@ -18,8 +18,7 @@ $cookie_value = "johnny5";
 
      else{
        echo "Welcome. This is your first time here...";
-       setcookie($cookie_name, $cookie_value, time() + (60), "/"); //86400 is the number of seconds in a day ....the slash means if it is multiple directories we can read it...86400 * 30
-
+       setcookie($cookie_name, $cookie_value, time() + (60), "/"); //86400 * 30 is 30 days...we currently have it set to 60 seconds...86400 is the number of seconds in a day ....the slash means if it is multiple directories we can read it...
      }
 
      ?>
