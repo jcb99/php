d97f176ab2c18cb48d9ae11cc9ea700dfea81f75
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-05 21:43:28 -0400

    push

--- cookies.php
+++ cookies.php
@@ -20,12 +20,8 @@ $last_visited_cookie_val = date("F j, Y, g:i a");
      {
        $last_visited = $_COOKIE['lastvisit'];
        setcookie($last_visited_cookie, $last_visited_cookie_val, 31536000 + time(), "/");
-
-       if (isset ($last_visited))
-          {
        echo "You have been here before... Your last visit was..." . $last_visited ;
-       //setcookie($last_visited_cookie, $last_visited_cookie_val, time() + (86400), "/");
-     }
+
    }
 
      else{
