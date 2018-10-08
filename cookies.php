220450da806f507f37c8e0fc9794c1c221f00798
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-08 11:46:30 -0400

    push

--- cookies.php
+++ cookies.php
@@ -18,7 +18,7 @@ $last_visited_cookie_val = date("F j, Y, g:i a");
 
      if (isset ($_COOKIE['user']) && (isset ($_COOKIE['lastvisit']))) //cookie name variable in the brackets
      {
-       $seconds = (date() + time()) - $last_visited;
+       $seconds = (date() - time()) - $last_visited;
        $last_visited = $_COOKIE['lastvisit'];
 
        echo "You have been here before... Your last visit was..." . $last_visited ;
