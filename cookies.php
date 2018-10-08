a6f46d534ebc0d08efd61d370ee50f5dfee30144
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-08 17:26:28 -0400

    push

--- cookies.php
+++ cookies.php
@@ -28,7 +28,7 @@ $time_cookie_val = time();
 
        echo "You have been here before... Your last visit was..." . $last_visited ;
        echo "<br /><br />";
-       echo "It has been " . $dt1 . " seconds since the last time you visited.";
+       echo "The following amount of time has passed since your last visit:<br>";
        setcookie($last_visited_cookie, $last_visited_cookie_val, 31536000 + time(), "/");
        setcookie($cookie_name, $cookie_value, time() + (31536000), "/");
        setcookie($time_cookie, $time_cookie_val, time() + (31536000), "/");
