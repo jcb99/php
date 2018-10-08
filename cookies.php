bdda59baca2b08cd2ca72f7b7e52de6b537769bd
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-08 16:22:43 -0400

    push

--- cookies.php
+++ cookies.php
@@ -18,6 +18,7 @@ $last_visited_cookie_val = date("F j, Y, g:i a");
 
      if (isset ($_COOKIE['user']) && (isset ($_COOKIE['lastvisit']))) //cookie name variable in the brackets
      {
+       $starttime = microtime(true);
 $endtime = microtime(true);
 $timediff = $endtime - $starttime;
 
@@ -31,7 +32,7 @@ $timediff = $endtime - $starttime;
        echo "It has been " . $timediff . " seconds since the last time you were here.";
        setcookie($last_visited_cookie, $last_visited_cookie_val, 31536000 + time(), "/");
        setcookie($cookie_name, $cookie_value, time() + (31536000), "/");
-       $starttime = microtime(true);
+
 
    }
 
