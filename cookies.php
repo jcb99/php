32e177d9c4a14acecebcadd1b7eeec4b5166729c
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-08 18:26:12 -0400

    push

--- cookies.php
+++ cookies.php
@@ -39,6 +39,7 @@ $time_cookie_val = time();
        echo "Welcome. This is your first time here...";
        setcookie($cookie_name, $cookie_value, time() + (31536000), "/"); //86400 * 30 is 30 days...we currently have it set to 60 seconds...86400 is the number of seconds in a day ....the slash means if it is multiple directories we can read it...
        setcookie($last_visited_cookie, $last_visited_cookie_val, 31536000 + time(), "/");
+       setcookie($time_cookie, $time_cookie_val, time() + (31536000), "/");
      }
 
      function convert_seconds($seconds_diff)
