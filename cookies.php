c702d427df458d6f257269e6327c6dd8b70bd18a
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-08 20:03:36 -0400

    push

--- cookies.php
+++ cookies.php
@@ -8,12 +8,7 @@ $last_visited_cookie_val = date("F j, Y, g:i a");
 $time_cookie = "time";
 $time_cookie_val = time();
 
-function convert_seconds($seconds_diff)
- {
-  $first = new DateTime("@0");
-  $second = new DateTime("@$seconds_diff");
-  return $first->diff($second)->format('%a days, %h hours, %i minutes and %s seconds');
-  }
+
  ?>
 
  <!DOCTYPE html>
@@ -35,9 +30,6 @@ function convert_seconds($seconds_diff)
        echo "You have been here before... Your last visit was..." . $last_visited ;
        echo "<br /><br />";
        echo "The following amount of time has passed since your last visit:<br>";
-       if ($old_time != 0){
-         echo convert_seconds($seconds_diff);
-      }
        setcookie($last_visited_cookie, $last_visited_cookie_val, 31536000 + time(), "/");
        setcookie($cookie_name, $cookie_value, time() + (31536000), "/");
        setcookie($time_cookie, $time_cookie_val, time() + (31536000), "/");
@@ -55,9 +47,17 @@ function convert_seconds($seconds_diff)
        setcookie($time_cookie, $time_cookie_val, time() + (31536000), "/");
      }
 
+     function convert_seconds($seconds_diff)
+      {
+       $first = new DateTime("@0");
+       $second = new DateTime("@$seconds_diff");
+       return $first->diff($second)->format('%a days, %h hours, %i minutes and %s seconds');
+       }
 
 
 
+         echo convert_seconds($seconds_diff);
+      
      ?>
    </body>
  </html>
