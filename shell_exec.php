ffe572e87d7da5a0defefc21dfc91e250428a4dc
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-10 11:21:19 -0400

    push

--- shell_exec.php
+++ shell_exec.php
@@ -4,4 +4,12 @@ echo "<pre>$output</pre>"; //displays the code as it appears in the terminal inc
 
 $pwd = shell_exec('pwd');
 echo "<pre>$pwd</pre>";
+
+if (is_dir('test')){
+  echo "This directory exists!";
+}
+
+else{
+  echo "This is not a directory!";
+}
  ?>
