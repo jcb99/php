8a6e67f8e84ecc5ca864accf323c73b04be82e93
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-10 11:42:33 -0400

    push

--- shell_exec.php
+++ shell_exec.php
@@ -3,7 +3,7 @@ $output = shell_exec('ls -lah');
 echo "<pre>$output</pre>"; //displays the code as it appears in the terminal including spacing....the <code> tag strips spaces
 
 $pwd = shell_exec('pwd');
-str_replace($pwd, " ", "");
+str_replace(" ", "", $pwd);
 echo "<pre>$pwd</pre>";
 
 echo $pwd. "/" . "test";
