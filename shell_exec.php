78f7dcbdf2f240b94f1de998f7f0dde4fa3a9d6a
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-11 10:14:47 -0400

    push

--- shell_exec.php
+++ shell_exec.php
@@ -6,14 +6,14 @@ $pwd = shell_exec('pwd');
 $pwd = rtrim($pwd);
 echo "<pre>$pwd</pre>";
 
-echo $pwd . '/' . 'tested';
-if (is_dir($pwd . '/' . 'tested')){
+echo $pwd . '/' . 'test';
+if (is_dir($pwd . '/' . 'test')){
   echo "<br>This directory exists!";
 }
 
 else{
   echo "This is not a directory!";
-  exec('mkdir tested');
+  exec('mkdir test');
   echo "We have created it for you!";
 }
  ?>
