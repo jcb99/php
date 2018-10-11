5244c39bd082b68a1467722022a45ab2629efb1a
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-11 10:42:45 -0400

    psub

---
+++ shell_exec_test.php
@@ -0,0 +1,31 @@
+<?php
+$output = shell_exec('ls -lah');
+echo "<pre>$output</pre>"; //displays the code as it appears in the terminal including spacing....the <code> tag strips spaces
+
+$pwd = shell_exec('pwd');
+$pwd = rtrim($pwd);
+echo "<pre>$pwd</pre>";
+
+echo $pwd . '/' . 'test';
+if (is_dir($pwd . '/' . 'test')){
+  echo "<br>This directory exists!<br>";
+
+  $dirArray = scandir("test/");
+
+  $hi = shell_exec("who");
+  echo $hi;
+
+  //var_dump($dirArray);
+  echo "<br><br>The following is a list of all of the items in this directory:<br>";
+  foreach ($dirArray as $key => $value) {  //Goes through each array element of dirArray...should be the number in the index
+    if ($value == "." || $value == ".."){continue;}
+    echo $value . "<br>";
+  }
+}
+
+else{
+  echo "<br>This is not a directory!";
+  exec('mkdir test');
+  echo "<br>We have created it for you!<br>";
+}
+ ?>
