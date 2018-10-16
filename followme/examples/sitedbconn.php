fa2ebf67e14a9b22fc70069b2bf7e13849306ceb
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-16 16:02:17 -0400

    push

--- followme/examples/sitedbconn.php
+++ followme/examples/sitedbconn.php
@@ -2,5 +2,5 @@
 
 $connection = new mysqli("localhost", "jake", "southhills#", "jake");
 if($connection->connect_error){
-  die("Cannot connect: " . $connection->connect_error)
+  die("Cannot connect: " . $connection->connect_error);
 }?>
