3e4ac7a5db16a94a79e517a4b4683675e046eed7
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-16 10:45:53 -0400

    push

--- followme/index.php
+++ followme/index.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+if(isset ($_POST['username'])){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $sql = "SELECT username, password FROM users WHERE username = '$username'";
+
+
+  header('Location: login.php');
+} ?>
+
 <!doctype html>
 <html lang="en">
 
