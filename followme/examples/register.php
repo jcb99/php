60503d10b7aac940e68659bdfc237e3184a15172
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-16 17:10:12 -0400

    push

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -9,12 +9,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   $email = preg_replace("/\s+/", "", $email);
 
   if($email!=""){
-    $email = $_POST['email'];
-    $email = filter_var($email, FILTER_SANITIZE_STRING);
-    $email = trim($email);
-    $email = str_replace("\\", "", $email);
-    $email = str_replace("/", "", $email);
-    $email = preg_replace("/\s+/", "", $email);
+    // $email = $_POST['email'];
+    // $email = filter_var($email, FILTER_SANITIZE_STRING);
+    // $email = trim($email);
+    // $email = str_replace("\\", "", $email);
+    // $email = str_replace("/", "", $email);
+    // $email = preg_replace("/\s+/", "", $email);
 
 
   $password = $_POST['password'];
