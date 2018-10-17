31c6a344ffaf289dc35568e19799108409953608
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-17 19:35:23 -0400

    push

--- followme/examples/register.php
+++ followme/examples/register.php
@@ -3,7 +3,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   require('sitedbconn.php');
   $email = $_POST['email'];
   //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-  $email = trim($email);
+  //$email = trim($email);
   $email = str_replace("\\", "", $email);
   $email = str_replace("/", "", $email);
   $email = preg_replace("/\s+/", "", $email);
