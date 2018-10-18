145e1fbab9f84f222567ef3cba27876c3b106099
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-17 20:45:59 -0400

    push

--- followme/examples/login.php
+++ followme/examples/login.php
@@ -15,7 +15,7 @@ if(isset ($_POST['email'])){
   $sql = "SELECT email, password FROM fm_users WHERE email = '$email'";
   $result = $conn->query($sql);
 
-  if (mysqli_num_rows($result) > 0){
+  if (mysqli_num_rows($result) > 0 && email !=""){
 
 
     while ($row = $result->fetch_assoc()){
