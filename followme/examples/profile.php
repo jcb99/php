f7fd9ae5f198a31fb852c18ebb3f8baffb26bd1c
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-18 10:51:13 -0400

    push

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); //Start session_start
 
-//Uses the $_SESSION['email'] POST variable to display the email in the nav bar at the top (Welcome $_SESSION['email'])
+//Uses the $_SESSION['email'] POST variable to display the email in the nav bar at the top (Welcome $_SESSION['image_url'])
 
 //Modify fm_users to image_url...load it to the $_SESSION['email'] variable
 
