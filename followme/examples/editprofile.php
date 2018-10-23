d491d80d160f8cd94a921684db0e93e45429a2f0
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-23 10:38:07 -0400

    push

--- followme/examples/editprofile.php
+++ followme/examples/editprofile.php
@@ -74,27 +74,32 @@ session_start(); //Start session_start
 						<div class="row">
 								<div class="col-md-8 ml-auto mr-auto">
 										<h2 class="text-center">Edit Profile</h2>
-										<form class="contact-form">
+										<form class="contact-form" action="" method="post">
 												<div class="row">
+
+
 														<div class="col-md-6">
 																<label>Name</label>
-						<div class="input-group">
+																<div class="input-group">
 																	<span class="input-group-addon">
 																			<i class="nc-icon nc-single-02"></i>
 																	</span>
 																	<input type="text" class="form-control" placeholder="Name">
-															</div>
+																</div>
 														</div>
+
+
 														<div class="col-md-6">
 																<label>Email</label>
-						<div class="input-group">
-							<span class="input-group-addon">
-								<i class="nc-icon nc-email-85"></i>
-							</span>
-							<input type="text" class="form-control" placeholder="Email">
-						</div>
+																<div class="input-group">
+																	<span class="input-group-addon">
+																		<i class="nc-icon nc-email-85"></i>
+																	</span>
+																	<input type="text" class="form-control" placeholder="Email">
+																</div>
 														</div>
-												</div>
+
+												</div> <!--Ends the first row -->
 												<label>Message</label>
 												<textarea class="form-control" rows="4" placeholder="Tell us your thoughts and feelings..."></textarea>
 												<div class="row">
