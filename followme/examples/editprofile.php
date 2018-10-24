4706a89672b06a8322dffb8b6f40b21ca58022cc
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-24 17:13:01 -0400

    push

--- followme/examples/editprofile.php
+++ followme/examples/editprofile.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION)){
 //Update submitted values to SQLiteDatabase
 //Update submitted value to $_SESSION
 if (isset($_POST['savebutton']))   {
-	$first_name=$_POST['first_name'];
+	$first_name=$_SESSION['first_name'];
 	$email=$_SESSION['email'];
 	$updatedb="UPDATE fm_users SET first_name=\"" .  $first_name . "\" WHERE email = " . $_SESSION['email'];
 	$conn->query($updatedb);
