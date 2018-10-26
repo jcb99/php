b13fa696ee9b0034b5f2002c9f892fd4a458d3c9
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-26 10:26:25 -0400

    push

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -90,7 +90,7 @@ if (!isset($_SESSION)){
                                                                         </div>
                                                                 </div>
                                                         </div>
-                                                </li>"
+                                                </li>";
                                               }?>
                                                 <hr />
                                         </ul>
