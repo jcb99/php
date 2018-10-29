975aa796206d83547bea696a9322e57a070b5539
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-29 19:43:57 -0400

    push

--- followme/examples/usersdup.php
+++ followme/examples/usersdup.php
@@ -79,7 +79,7 @@ if (!isset($_SESSION)){
                                                                         <img alt="Circle Image" class="img-circle img-no-padding img-responsive">
                                                                 </div>
                                                                 <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                                                                       <h6> <?php echo $row['first_name'] . ' ' . $row['last_name']; ?> <br/><small> <?php echo $row['title']; ?>  </small></h6>
+                                                                       <h6> <?php echo $row['first_name'] . " " . $row['last_name']; ?> <br/><small> <?php echo $row['title']; ?>  </small></h6>
                                                               </div>
                                                                 <div class="col-md-3 col-sm-2  ml-auto mr-auto">
                                                                         <div class="form-check">
