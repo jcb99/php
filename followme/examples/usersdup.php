3b41eb70373b7c3662c60b67822a614af7eff11f
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-10-29 20:55:50 -0400

    push

--- followme/examples/usersdup.php
+++ followme/examples/usersdup.php
@@ -89,7 +89,7 @@ if (!isset($_SESSION)){
                                                                 <div class="col-md-3 col-sm-2  ml-auto mr-auto">
                                                                         <div class="form-check">
                                                                                 <label class="form-check-label">
-                                                                                        <input class="form-check-input" type="checkbox" value="" <?php if (mysqli_num_rows($follows_result) > 0){echo "checked";} ?>>
+                                                                                        <input class="form-check-input" type="checkbox" value="" <?php if (mysqli_num_rows($follows_result) > 0){echo "checked";} else{echo "unchecked";} ?>>
                                                                                         <span class="form-check-sign"></span>
                                                                                 </label>
                                                                         </div>
