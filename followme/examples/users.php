00276963767e7f99a9ce5427b13c0744329c281a
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-11-04 13:51:12 -0500

    hmm

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -117,6 +117,7 @@ if (!isset($_SESSION)){
 																								}
 																					  function IsChecked($check_follow,$value)
 																					    {
+																								require('sitedbconn.php')
 
 																					        if(!empty($_POST[$check_follow]))
 																					        {
