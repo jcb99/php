e966568053cfed1751a90164f1bbd57097efe18c
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-11-04 09:37:52 -0500

    push

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -1,12 +1,7 @@
 <?php
 if (!isset($_SESSION)){
 	session_start(); //Start session_start
-}
-
-$followers="SELECT * FROM fm_follows;";
-$qresult = $conn->query($followers);
-
- ?>
+} ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -72,33 +67,42 @@ $qresult = $conn->query($followers);
                                         <ul class="list-unstyled follows">
                                           <?php
                                           require('sitedbconn.php');
-                                          $sql = "SELECT * FROM fm_users";
+                                          $followsql = "SELECT * FROM fm_users";
+
+																					$thisuser=$_SESSION['user_id'];
+																					$follows="SELECT followed_user FROM fm_follows WHERE followed_by=$thisuser";
 
                                           //Execute the SQL Query
-                                          $result = $conn->query($sql);
+                                          $theresult = $conn->query($followsql);
+																					$follows_result=$conn->query($follows);
 
-                                            while ($row = $result->fetch_assoc()) {
-                                                echo "<li>
-                                                        <div class=\"row\">
-                                                                <div class=\"col-md-2 col-sm-2 ml-auto mr-auto\">
-                                                                        <img src=\"". $row['image_url'] . "  \"alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">
+																					while($row = $follows_result->fetch_assoc()){
+																					 $follow_array[]=$row['followed_user'];
+																					 }
+
+                                             while ($row = $theresult->fetch_assoc()) { ?>
+                                                <li>
+                                                        <div class="row">
+                                                                <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                                                                        <img src="<?php echo $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
                                                                 </div>
-                                                                <div class=\"col-md-7 col-sm-4  ml-auto mr-auto\">";
-                                                                      echo  "<h6>" . $row['first_name'] . ' ' . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                                              echo "</div>
-                                                                <div class=\"col-md-3 col-sm-2  ml-auto mr-auto\">
-                                                                        <div class=\"form-check\">
-                                                                                <label class=\"form-check-label\">";?>
-                                                                                        <input name="followbox" class="form-check-input" type="checkbox" value="" >
-
-                                                                                      <span class="form-check-sign"></span>
+                                                                <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                                                                       <h6> <?php echo $row['first_name'];
+																																			 echo " ";
+																																			 echo $row['last_name']; ?> <br/><small> <?php echo $row['title']; ?>  </small></h6>
+                                                              </div>
+                                                                <div class="col-md-3 col-sm-2  ml-auto mr-auto">
+                                                                        <div class="form-check">
+                                                                                <label class="form-check-label">
+                                                                                        <input class="form-check-input" type="checkbox" value="" <?php if (in_array($row['user_id'], $follow_array)){echo "checked";} ?>>
+                                                                                        <span class="form-check-sign"></span>
                                                                                 </label>
                                                                         </div>
                                                                 </div>
                                                         </div>
                                                 </li>
                                                 <hr />
-                                              }
+																								<?php }?>
                                                 <hr />
                                         </ul>
                                 </div>
