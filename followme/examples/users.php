16398df0b642c19fa196fbcb7001bf0eb0afec64
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-11-06 15:53:18 -0500

    yeah

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -5,28 +5,18 @@ if (!isset($_SESSION)){
 
 function IsChecked($check_follow,$value){
 require('sitedbconn.php');
-
-	// if(!empty($_POST[$check_follow]))
-	// {
 		if (in_array($value, $_POST[$check_follow]))
 		{
 			return true;
 		}
-	// }
-	return false;
 }
 
 function IsUnchecked($check_follow,$value){
 require('sitedbconn.php');
-
-	// if(!empty($_POST[$check_follow]))
-	// {
 		if (!in_array($value, $_POST[$check_follow]))
 		{
 			return true;
 		}
-	// }
-	return false;
 }
 
 ?>
