e6cead0fd0ee0417cf26e0b4fd5278baabed3037
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-11-08 17:20:02 -0500

    hmm

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -177,7 +177,6 @@ require('sitedbconn.php');
 																				<div class="col-md-3 col-sm-2  ml-auto mr-auto">
 																								<div class="form-check">
 																												<label class="form-check-label">
-																																<input class="form-check-input" type="checkbox" value="<?php echo $row['user_id'] ?>" name="follow_checkbox[]" <?php if (in_array($row['user_id'], $follow_array)){echo "checked";} ?>>
 																																<span class="form-check-sign"></span>
 																												</label>
 																								</div>
