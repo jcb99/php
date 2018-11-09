43371b92f84b9ae050b09ff08a59f1570d417927
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-11-09 10:03:06 -0500

    push

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -190,7 +190,6 @@ require('sitedbconn.php');
 									if ($follow_array==""){
 											echo "<h3 class=\"text-muted\">Not following anyone yet :(</h3>";
 										}?>
-                        <button class="btn btn-warning btn-round">Find artists</button>
                     </div>
                 </div>
             </div>
