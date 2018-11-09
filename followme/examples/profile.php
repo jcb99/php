f063985606f5b8fec4f4314ed8272c39ce6bdf36
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-11-09 10:14:01 -0500

    push

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -92,7 +92,7 @@ require('sitedbconn.php');
                     <div class="nav-tabs-wrapper">
                         <ul class="nav nav-tabs" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Follows</a>
+                                <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Followers</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" data-toggle="tab" href="#following" role="tab">Following</a>
@@ -102,51 +102,63 @@ require('sitedbconn.php');
                 </div>
                 <!-- Tab panes -->
                 <div class="tab-content following">
-                    <div class="tab-pane active" id="follows" role="tabpanel">
-                        <div class="row">
-                            <div class="col-md-6 ml-auto mr-auto">
-                                <ul class="list-unstyled follows">
-                                    <li>
-                                        <div class="row">
-                                            <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-                                                <img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                            </div>
-                                            <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                                                <h6>Flume<br/><small>Musical Producer</small></h6>
-                                            </div>
-                                            <div class="col-md-3 col-sm-2  ml-auto mr-auto">
-												<div class="form-check">
-					                                <label class="form-check-label">
-					                                    <input class="form-check-input" type="checkbox" value="" checked>
-					                                    <span class="form-check-sign"></span>
-					                                </label>
-					                            </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <hr />
-                                    <li>
-                                        <div class="row">
-                                            <div class="col-md-2 ml-auto mr-auto ">
-                                                <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                            </div>
-                                            <div class="col-md-7 col-sm-4">
-                                                <h6>Banks<br /><small>Singer</small></h6>
-                                            </div>
-                                            <div class="col-md-3 col-sm-2">
-												<div class="form-check">
-					                                <label class="form-check-label">
-					                                    <input class="form-check-input" type="checkbox" value="">
-					                                    <span class="form-check-sign"></span>
-					                                </label>
-					                            </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+
+
+
+
+
+										<div class="tab-pane" id="follows" role="tabpanel">
+											<?php
+											$thisuser=$_SESSION['user_id'];
+
+											$followsql = "SELECT * FROM fm_users";
+											$follows="SELECT followed_by FROM fm_follows WHERE followed_user=$thisuser";
+
+											$theresult = $conn->query($followsql);
+											$follows_result=$conn->query($follows);
+
+											while($row = $follows_result->fetch_assoc()){
+											 $follow_array[]=$row['followed_user'];
+											 }
+
+											 while ($row = $theresult->fetch_assoc()) {
+											 if (in_array($row['user_id'], $follow_array)){
+												?>
+												<div class="row">
+																<div class="col-md-6 ml-auto mr-auto">
+																	<ul class="list-unstyled follows">
+												<li>
+																<div class="row">
+																				<div class="col-md-2 col-sm-2 ml-auto mr-0">
+																								<img src="<?php echo $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+																				</div>
+																				<div class="col-md-7 col-sm-4  ml-0 mr-0">
+																							 <h6> <?php echo $row['first_name'];
+																							 echo " ";
+																							 echo $row['last_name']; ?> <br/><small> <?php echo $row['title']; ?>  </small></h6>
+																			</div>
+
+																</div>
+												</li>
+												<hr>
+											</ul>
+										</div>
+										</div>
+
+											<?php }
+										}
+										if ($follow_array==""){
+											echo "<h3 class=\"text-muted\">Not following anyone yet :(</h3>";
+										}?>
+										</div>
+
+
+
+
+
+
+
+
                     <div class="tab-pane" id="following" role="tabpanel">
 											<?php
 											$thisuser=$_SESSION['user_id'];
